2ea0f194ad78478f7e6d6f14892781f966f9adbe
TheRealKevBot <Kglas001@gmail.com>
2021-07-30 20:30:23 -0600

    First

---
+++ index.js
@@ -0,0 +1,13 @@
+function sumOfCubes(nums) {
+
+}
+
+console.log(sumOfCubes([1, 5, 9]), 855)
+console.log(sumOfCubes([3, 4, 5]), 216)
+console.log(sumOfCubes([1, 1, 1]), 3)
+console.log(sumOfCubes([2]), 8)
+console.log(sumOfCubes([5, 1, 2]), 134)
+console.log(sumOfCubes([32]), 32768)
+console.log(sumOfCubes([5, 9, 4, 4, 9]), 1711)
+console.log(sumOfCubes([0, 1, 2]), 9)
+console.log(sumOfCubes([]), 0)
